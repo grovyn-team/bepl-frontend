cf210ee33ce42f924dd67f5512159f5d0ab92eb3
Gaurav Singh <thakurgaurav273@gmail.com>
2025-12-29 03:30:00 +0530

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,8 +19,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
-        display: ["Space Grotesk", "Inter", "system-ui", "sans-serif"],
+        sans: ["Montserrat", "system-ui", "sans-serif"],
+        display: ["Montserrat", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
